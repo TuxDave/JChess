44a9c9937e84d18a1bc6e52a9f8079f419a49b8e
TuxDave <godinodavide@gmail.com>
2020-07-16 09:58:36 +0200

    added a cell hilighter method

--- src/main/java/com/tuxdave/JChess/UI/GraphicalBoard.java
+++ src/main/java/com/tuxdave/JChess/UI/GraphicalBoard.java
@@ -1,14 +1,14 @@
 package com.tuxdave.JChess.UI;
 
+import com.tuxdave.JChess.core.GameBoard;
 import com.tuxdave.JChess.extras.Vector2;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
 
 public class GraphicalBoard extends JComponent {
+
+    private Vector2[] selectedCells = {};
 
     {//static properties
         setBorder(BorderFactory.createLineBorder(Color.blue));
@@ -43,10 +43,60 @@ public class GraphicalBoard extends JComponent {
                 p = 0;
             }
         }
+
+        //this hilight some cells
+        highlightCell(g);
+        //todo: add the method that update the array containing the list of the selected cells
+
+        //todo: add the method that paints the pieces on screen
     }
 
     /**
-     * this method convert a matematic y coordinate in a swing coordinates (so using 0,0 on left-top) and a place of 512x512
+     * paints green color on the selected cells
+     */
+    private void highlightCell(Graphics g){
+        Image img = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Resources/Icons/selected_background.png"));
+        for(Vector2 _cell : selectedCells){
+            if (isAnAcceptableCell(_cell)) {
+                _cell = getPixelCoordsFromCellCoords(_cell);
+                _cell.y = convertCoordsFromReal(_cell.y);
+                g.drawImage(img, _cell.x, _cell.y, this);
+            } else {
+                throw new IllegalArgumentException("Cell not found!");
+            }
+        }
+    }
+
+    /**
+     * @param _cell the cell to tests
+     * @return true if the cell is acceptable (between the board limits)
+     */
+    private static boolean isAnAcceptableCell(Vector2 _cell){
+        if(_cell.isBetweenLimits(0,0,GameBoard.limits, GameBoard.limits)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    /**
+     * @param _x the cell's x to tests
+     * @param _y the cell's y to tests
+     * @return true if the cell is acceptable (between the board limits)
+     */
+    private static boolean isAnAcceptableCell(int _x, int _y){
+        return isAnAcceptableCell(new Vector2(_x, _y));
+    }
+
+    /**
+     * @param _cellCoords cell's coords to tranform in the coords of the pixel on north-ovest of the cell size
+     * @return a Vector2 containing the coords by pixel
+     */
+    private static Vector2 getPixelCoordsFromCellCoords(Vector2 _cellCoords){
+        return new Vector2((_cellCoords.x-1)*64, _cellCoords.y*64);
+    }
+
+    /**
+     * this method converts a matematic y coordinate in a swing coordinates (so using 0,0 on left-top) and a place of 512x512
      * @param _y geometric coords using 0,0 on left-bottom
      * @return the new y converted
      */
--- src/main/java/com/tuxdave/JChess/extras/Vector2.java
+++ src/main/java/com/tuxdave/JChess/extras/Vector2.java
@@ -31,4 +31,30 @@ public class Vector2 {
             return false;
         }
     }
+
+    /**
+     * WARNING: the numbers indicated as minimum and maximum are included and considered as possible
+     * @param _min the minimum possible value of x and y
+     * @param _max the maximum possible value of x and y
+     * @return true if the vector is between the limits
+     */
+    public boolean isBetweenLimits(Vector2 _min, Vector2 _max){
+        if(x >= _min.x && y >= _min.y && x <= _max.x && y <= _max.y){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * WARNING: the numbers indicated as minimum and maximum are included and considered as possible
+     * @param x1 minimum x value
+     * @param y1 minimum y value
+     * @param x2 maximum x value
+     * @param y2 maximum y value
+     * @return true if the vector is between the limits
+     */
+    public boolean isBetweenLimits(int x1, int y1, int x2, int y2){
+        return isBetweenLimits(new Vector2(x1,y1), new Vector2(x2,y2));
+    }
 }
