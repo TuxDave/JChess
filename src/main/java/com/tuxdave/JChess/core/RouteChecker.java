c68caec30f02b9638503a289271a39f4a008de0a
TuxDave <godinodavide@gmail.com>
2020-07-22 18:06:23 +0200

    intermediate commit, created RouteChecker, not implemented

---
+++ src/main/java/com/tuxdave/JChess/core/RouteChecker.java
@@ -0,0 +1,17 @@
+package com.tuxdave.JChess.core;
+
+import com.tuxdave.JChess.core.pieces.Pezzo;
+import com.tuxdave.JChess.extras.Vector2;
+
+/**
+ * class that will check the pieces route on moving and stop it if a orute is occupied
+ */
+public class RouteChecker {
+
+    /**todo: implements this
+     * @param p the piece from which calculate the possible moves in base of routes
+     * @param board the game board used
+     * @return an array containing all the possible moves
+     */
+    public static Vector2[] getPossibleMoves(Pezzo p, GameBoard board){return new Vector2[1];}
+}
